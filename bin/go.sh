b4c16ed1a58c20b184e952fed617e35f2535976b
Sam Daniel <daniel.jacob.samuel@gmail.com>
2019-04-27 21:12:28 +0000

    Add go install script

---
+++ bin/go.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+source ../utils.sh
+VERSION="1.12.4"
+OS="linux"
+ARCH="amd64"
+GOSTR="go$VERSION.$OS-$ARCH"
+BUILDDIR=$(realpath ../build)
+echo -e "\n$(green)installing $GOSTR to $(blue)/usr/local$(reset)\n"
+pushd $BUILDDIR
+  wget https://dl.google.com/go/$GOSTR.tar.gz
+  tar -C /usr/local -xzf go$VERSION.$OS-$ARCH.tar.gz
+popd
+echo -e "\n$(green)done installing go$VERSION.$OS-$ARCH$(reset)\n"
