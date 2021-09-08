e195ba1311f08330704c042e5f9b921262ff7814
Sam Daniel <sam@firstmode.com>
2021-09-08 02:36:56 -0700

    Install Linux appimage of nvim

--- setup.sh
+++ setup.sh
@@ -51,3 +51,16 @@ mkdir -p $HOME/.config
 cp -r \
   $PWD/.config/* \
   $HOME/.config
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  echo "skipping nvim for macOS"
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  pushd $HOME
+    curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+    chmod u+x nvim.appimage
+    ./nvim.appimage --appimage-extract
+    ./squashfs-root/AppRun --version
+  popd
+else
+  echo "Unknown OSTYPE: ${OSTYPE}"
+fi
