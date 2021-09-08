e4bd4558fc42c70651c1be9988d59837189e310d
Sam Daniel <sam@firstmode.com>
2021-09-08 01:30:11 -0700

    Use /Users/sam in fzf.bash

--- .fzf.bash
+++ .fzf.bash
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/sam/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/sam/.fzf/bin"
+if [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}$HOME/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/sam/.fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source $HOME/.fzf/shell/completion.bash 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/sam/.fzf/shell/key-bindings.bash"
+source $HOME/.fzf/shell/key-bindings.bash
