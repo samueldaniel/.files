7bb1189c2f51da3e74f855565ba448a9f122d553
Samuel Daniel <sam@firstmode.com>
2022-03-08 20:19:19 -0800

    20220222 updates (#24)
    * Fix permissions on ssh config file
    
    * Add newline to prompt to make it easier to read and separate past commands
    
    * Add prospector to nvim linting config
    
    * Recurse into submodules by default
    
    * Dump nvim-python3 packages after installing prospector

--- setup.sh
+++ setup.sh
@@ -22,6 +22,7 @@ mkdir -p $HOME/.ssh
 cp -r \
   $PWD/.ssh/* \
   $HOME/.ssh
+chmod 600 $HOME/.ssh/config
 
 curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
