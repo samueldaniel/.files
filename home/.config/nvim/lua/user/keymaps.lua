fccd939d9210cbb98717140aab4a1be35ea582be
Sam Daniel <sam@firstmode.com>
2023-03-26 16:37:57 -0700

    more fixes and tweaks

--- home/.config/nvim/lua/user/keymaps.lua
+++ home/.config/nvim/lua/user/keymaps.lua
@@ -4,6 +4,35 @@ map("n", "<C-p>", function()
   require("telescope.builtin").find_files()
 end, { desc = "Telescope search files" })
 
+--map("n", "<C-b>", function()
+--  require("telescope.builtin").buffers()
+--end, { desc = "Telescope search buffers" })
+
+map("n", "<C-g>", function()
+  require("telescope.builtin").live_grep()
+end, { desc = "Telescope live grep" })
+
+map("n", "<C-b>", function(opts)
+  local action_state = require('telescope.actions.state')
+  local actions = require('telescope.actions')
+  opts = opts or {}
+  opts.attach_mappings = function(prompt_bufnr, map)
+    local delete_buf = function()
+      local selection = action_state.get_selected_entry()
+      actions.close(prompt_bufnr)
+      vim.api.nvim_buf_delete(selection.bufnr, { force = true })
+    end
+    map('i', '<c-u>', delete_buf)
+    return true
+  end
+  opts.previewer = false
+  -- define more opts here
+  -- opts.show_all_buffers = true
+  -- opts.sort_lastused = true
+  -- opts.shorten_path = false
+  require('telescope.builtin').buffers(require('telescope.themes').get_dropdown(opts))
+end, { desc = "foo"})
+
 map("n", "<C-e>", ":NvimTreeToggle<cr>", {desc = "NvimTreeToggle"})
 -- vim.api.nvim_set_keymap(
 --   'n',
